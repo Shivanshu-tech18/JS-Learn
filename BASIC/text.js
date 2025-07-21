abf5ebba8d7847d7942e64d7a2329080cd76f26d
Shivanshu-tech18 <2k23.cs2312795@gmail.com>
2025-07-21 03:27:39 +0000

    variables

--- BASIC/text.js
+++ BASIC/text.js
@@ -1,1 +1,13 @@
 console.log("Shivanshu Tripahti ")
+
+const accountID=12345;
+let accountEmail="abcxyz@gamil.com";
+var accountPassword="122346";
+accountCity="jaipur"
+
+//accountID=2
+console.log(accountID)
+accountEmail="ahdhakslsl@gmail.com"
+accountCity="kanpur"
+accountPassword="894567"
+console.table([accountEmail,accountID,accountPassword,accountCity])
