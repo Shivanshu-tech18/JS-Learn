d2fe4adbe63bb08a64038c0e58a83081c6d1524a
Shivanshu-tech18 <2k23.cs2312795@gmail.com>
2025-07-21 04:33:31 +0000

    conversion comparison

--- BASIC/conversion.js
+++ BASIC/conversion.js
@@ -1,12 +1,23 @@
-let score="33"
-console.log(typeof(score))
+// let score="33"
+// console.log(typeof(score))
 
-let valueinNumber=Number(score)
-console.log(typeof valueinNumber)
+// let valueinNumber=Number(score)
+// console.log(typeof valueinNumber)
 
-let value=true
-console.log(typeof value)
+// let value=true
+// console.log(typeof value)
 
-let valueinvalue=Number(true)
-console.log(valueinvalue)
-console.log(typeof valueinvalue)
+// let valueinvalue=Number(true)
+// console.log(valueinvalue)
+// console.log(typeof valueinvalue)
+
+let s1="hello";
+let s2 =" world!";
+let s3=s1+s2;
+console.log(s3);
+console.log("1"+12)//112
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+console.log(2=="2")//true it converts data type and then checks
+console.log(2==="2")//false it checks strictly with datatype
