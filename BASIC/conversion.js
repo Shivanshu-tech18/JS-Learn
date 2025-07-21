ede7c91b4c57c8b1e73739ef6db3dd9c5e348cf3
Shivanshu-tech18 <2k23.cs2312795@gmail.com>
2025-07-21 04:18:21 +0000

    conversion of datatypes

---
+++ BASIC/conversion.js
@@ -0,0 +1,12 @@
+let score="33"
+console.log(typeof(score))
+
+let valueinNumber=Number(score)
+console.log(typeof valueinNumber)
+
+let value=true
+console.log(typeof value)
+
+let valueinvalue=Number(true)
+console.log(valueinvalue)
+console.log(typeof valueinvalue)
