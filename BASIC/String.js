20a46113b861f3e8ac186f0ddee6ac0214d32a1c
Shivanshu-tech18 <2k23.cs2312795@gmail.com>
2025-07-22 04:57:24 +0000

    Stings in js

---
+++ BASIC/String.js
@@ -0,0 +1,16 @@
+console.log("hello world!");
+const name="Shivanshu";
+const repocount=5;
+console.log(name+ repocount+" ");
+
+console.log(name[0]);
+console.log(name.__proto__);
+console.log(name.toUpperCase());
+console.log(name.length);
+console.log(name.charAt(3));
+console.log(name.indexOf('n'));
+
+const newstr=name.substring(0,4);
+console.log(newstr)
+const anoth=name.slice(-8,-3)
+console.log(anoth)
