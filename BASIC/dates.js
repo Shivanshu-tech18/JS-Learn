7b0f8e9a2d7e38bd8bb82fd191291b9c24398f2f
Shivanshu-tech18 <2k23.cs2312795@gmail.com>
2025-07-27 03:50:11 +0000

    dates

---
+++ BASIC/dates.js
@@ -0,0 +1,20 @@
+let myDate=new Date();
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+
+let myCreatedDate=new Date(2025,2,15);
+// console.log(myCreatedDate.toLocaleDateString())
+
+let myTimeStamp=Date.now();
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+console.log(newDate.getMonth());
